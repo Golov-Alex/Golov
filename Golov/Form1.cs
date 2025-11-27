624455bddb51167b89d3b704944cf6437c346a1b
Golov_Alex <ynd3238@gmail.com>
2025-11-28 00:59:44 +0300

    Добавьте файлы проекта.

---
+++ Golov/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Entity;
+
+
+namespace Golov
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ModelEF model = new ModelEF();
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            StartLoadData();
+        }
+
+        private void bindingNavigatorDeleteItem_Click(object sender, EventArgs e)
+        {
+            SaveData();
+        }
+
+        private void bindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            SaveData();
+        }
+
+        private void bindingNavigatorAddNewItem_Click(object sender, EventArgs e)
+        {
+            role_NameComboBox.SelectedIndex = 0;
+        }
+
+        private void StartLoadData()
+        {
+            model.Users.Load();
+            role_NameComboBox.DataSource = model.Roles.ToList();
+            usersBindingSource.DataSource = model.Users.Local.ToBindingList();
+        }
+
+        private void SaveData()
+        {
+            try
+            {
+                Validate();
+                usersBindingSource.EndEdit();
+                usersBindingSource.ResetBindings(true);
+                model.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                StartLoadData();
+            }
+        }
+    }
+}
